f3eb5251093bd299712547c12f1362445ddf16cf
pulnara <magnes.agnieszka@gmail.com>
2021-06-11 01:26:09 +0200

    Add current efficiency to get Restaurant endpoint

--- ftgo-kitchen-service/src/main/java/net/chrisrichardson/ftgo/kitchenservice/web/GetRestaurantResponse.java
+++ ftgo-kitchen-service/src/main/java/net/chrisrichardson/ftgo/kitchenservice/web/GetRestaurantResponse.java
@@ -2,6 +2,7 @@ package net.chrisrichardson.ftgo.kitchenservice.web;
 
 public class GetRestaurantResponse  {
   private long restaurantId;
+  private long currentEfficiency;
 
   public long getRestaurantId() {
     return restaurantId;
@@ -11,11 +12,20 @@ public class GetRestaurantResponse  {
     this.restaurantId = restaurantId;
   }
 
+  public long getCurrentEfficiency() {
+    return currentEfficiency;
+  }
+
+  public void setCurrentEfficiency(long currentEfficiency) {
+    this.currentEfficiency = currentEfficiency;
+  }
+
   public GetRestaurantResponse() {
 
   }
 
-  public GetRestaurantResponse(long restaurantId) {
+  public GetRestaurantResponse(long restaurantId, long currentEfficiency) {
     this.restaurantId = restaurantId;
+    this.currentEfficiency = currentEfficiency;
   }
 }
